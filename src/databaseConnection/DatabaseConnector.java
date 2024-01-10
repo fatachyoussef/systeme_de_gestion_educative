b0cf048e74b3b055cfc2fdd16084997b8fdd4453
fatach_youssef <fatachyoussef9@gmail.com>
2024-01-10 17:47:59 +0100

    com

--- src/databaseConnection/DatabaseConnector.java
+++ src/databaseConnection/DatabaseConnector.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DatabaseConnector {
-    private static final String URL = "jdbc:mysql://localhost:3306/mydbjava";
+    private static final String URL = "jdbc:phpmyadmin://localhost:3306/mydb";
     private static final String USER = "root";
     private static final String PASSWORD = "";
     public static Connection getConnection() throws SQLException {
